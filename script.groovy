7ec52e3a19ea3d59fdced6340e10517fdc3c683a
WaqasSaleem47 <waqassaleem47@protonmail.com>
2025-05-23 11:01:05 +0500

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp(){
+  echo "App ${params.APP_ENV}..."
+  echo "Building the application..."
+  echo "Version ${params.VERSION}"
+}
+
+def testApp(){
+  echo "Testing the application..."
+}
+
+def deployApp(){
+  echo "Deploying the application..."
+}
