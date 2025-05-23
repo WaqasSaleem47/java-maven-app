9b0ffae11b3ee51d61ad62f16cb9055b3fc3c97c
WaqasSaleem47 <waqassaleem47@protonmail.com>
2025-05-23 18:49:14 +0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,3 +1,8 @@
+def runTest() {
+    echo "testing the application..."
+    sh 'mvn test'
+}
+
 def buildJar() {
     echo "building the application..."
     sh 'mvn package'
@@ -5,10 +10,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'dockerHub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t waqassaleem/java-maven-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push waqassaleem/java-maven-app:jma-2.0'
     }
 } 
 
