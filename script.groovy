7f7ee6d89ee2beabd506a5aea50660c1caae6fdb
WaqasSaleem47 <waqassaleem47@protonmail.com>
2025-06-03 18:45:01 +0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -12,7 +12,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'Dockerhub-Credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t waqassaleem/java-maven-app:jma-4.0 .'
-        sh 'docker tag localhost:90/java-maven-app:jma-4.0 waqassaleem/java-maven-app:jma-4.0'
+        sh 'docker tag waqassaleem/java-maven-app:jma-4.0 localhost:90/java-maven-app:jma-4.0'
         
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push waqassaleem/java-maven-app:jma-4.0'
