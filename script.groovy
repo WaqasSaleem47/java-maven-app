2184165ed33ba0f8ae1a3709aadd9cbefb75bb2a
WaqasSaleem47 <waqassaleem47@protonmail.com>
2025-06-03 18:01:00 +0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,27 +1,15 @@
-def runTest() {
-    echo "testing the application..."
-    sh 'mvn test'
+def buildApp(){
+  echo "App ${params.APP_ENV}..."
+  echo "Building the application..."
+  echo "Version ${params.VERSION}"
 }
 
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
+def testApp(){
+  echo "Testing the application..."
+}
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerHub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t waqassaleem/java-maven-app:4.0 .'
-        sh 'docker tag waqassaleem/java-maven-app:4.0 localhost:90/java-maven-app:4.0'
-        
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push waqassaleem/java-maven-app:4.0'
-        sh 'docker push localhost:90/java-maven-app:4.0'
-    }
-} 
-
-def deployApp() {
-    echo 'deploying the application...'
-} 
-
+def deployApp(){
+  echo "Deploying the application..."
+}
 return this
+
