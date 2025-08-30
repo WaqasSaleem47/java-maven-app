5fe8446710d6d26dd0b0b87042e920d6f63cd637
Admin <admin@gmail.com>
2025-08-30 12:24:51 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -3,17 +3,6 @@ def runTest() {
     sh 'mvn test'
 }
 
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
-
-def buildImage() {
-    echo "building the docker image..."
-    sh 'docker build -t waqassaleem/java-maven-app:4.0 .'
-    sh 'docker tag waqassaleem/java-maven-app:4.0 localhost:90/java-maven-app:4.0'
-} 
-
 def deployApp() {
     echo 'deploying the application...'
     withCredentials([usernamePassword(credentialsId: 'dockerHub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
