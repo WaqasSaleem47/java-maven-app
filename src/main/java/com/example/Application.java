05769421a2bbae321bfaccf960182618dc1045a8
Waqas Saleem <202511761+WaqasSaleem47@users.noreply.github.com>
2026-08-30 14:54:47 +0500

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -19,6 +19,7 @@ public class Application {
     {
         Logger log = LoggerFactory.getLogger(Application.class);
         log.info("Java app started");
+        log.info("successfully");
     }
 
     public String getStatus() {
