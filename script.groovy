d79291b88cb4f7b6d3f792672f0544588bbf622e
Waqas Saleem <202511761+WaqasSaleem47@users.noreply.github.com>
2026-08-30 15:56:11 +0500

    Refactor build and test functions in script.groovy

--- script.groovy
+++ script.groovy
@@ -1,27 +1,13 @@
-def runTest() {
-    echo "testing the application..."
-    sh 'mvn test'
+def buildApp() {
+  echo 'building the application...'
 }
 
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
-
-def buildImage() {
-    echo "building the docker image..."
-    sh 'docker build -t waqassaleem/java-maven-app:4.0 .'
-    sh 'docker tag waqassaleem/java-maven-app:4.0 localhost:90/java-maven-app:4.0'
-} 
+def testApp() {
+  echo 'testing the application...'
+}
 
 def deployApp() {
-    echo 'deploying the application...'
-    withCredentials([usernamePassword(credentialsId: 'dockerHub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push waqassaleem/java-maven-app:4.0'
-        sh 'docker push localhost:90/java-maven-app:4.0'
-    }
-} 
+  echo "deploying version ${params.VERSION}"
+}
 
 return this
-
